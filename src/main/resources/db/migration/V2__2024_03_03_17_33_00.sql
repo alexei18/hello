2208bbc33f34678b7056be08527142f221d3d24b
andy1kspn <andrei0spinu@gmail.com>
2024-03-21 20:12:09 +0200

    Version 2.1 - Added the image url field for the Book Entity

--- src/main/resources/db/migration/V2__2024_03_03_17_33_00.sql
+++ src/main/resources/db/migration/V2__2024_03_03_17_33_00.sql
@@ -13,8 +13,9 @@ create table books(
     id_author integer,
     type varchar(255),
     publication_year date,
+    image_url VARCHAR(255),
     foreign key (id_author) references author(id_author)
 );
 
 insert into books
-values (1,'Padurea Spanzuratilor',1,'Roman','2023-01-01');
+values (1,'Padurea Spanzuratilor',1,'Roman','2023-01-01','https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1382993429i/778267.jpg');
